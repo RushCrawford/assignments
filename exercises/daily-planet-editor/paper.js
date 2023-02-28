43ab0026bc2deed46dcbe80e97ff8134400cbdc7
Russell Crawford <acrawford0221@gmail.com>
2023-02-27 18:47:54 -0700

    finished assignment

--- exercises/daily-planet-editor/paper.js
+++ exercises/daily-planet-editor/paper.js
@@ -11,21 +11,19 @@ just fix the existing ones.
 
 var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
 
-function whoWins(isKryptonitePresent, enemyName)
-{
-if (isKryptonitePresent)
-{
-    return "Superman beats " +enemyName+ ", of course";
-}
-
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
-}
+function whoWins(isKryptonitePresent, enemyName){
+    if (isKryptonitePresent)
+    {
+        return "Superman beats " +enemyName+ ", of course";
     }
 
-for (var i = 0; i < enemies.length; i++)
+    else{
+        return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
+        }
+    }
 
-    var kryptonite {
+for (var i = 0; i < enemies.length; i++){
+    var kryptonite
     if (i % 2 === 0) 
     {
         isKryptonitePresent = true;
@@ -38,17 +36,17 @@ for (var i = 0; i < enemies.length; i++)
 function hotnessScale ()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return math.floor((math.random()*10)+1);
+    return Math.floor((Math.random()*10)+1);
 }
 
-consol.log( hotnessScale ()) ;
+console.log( hotnessScale ()) ;
 
 var clarkKent = true;
 var superman = false;
 
 while (clarkKent){
     console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange = math.random();
+    var phoneBoothQuickChange = Math.random();
     if (phoneBoothQuickChange >= 0.5) {
         clarkKent = false;
         superman = true;
