f34c25c87adb177f95d57c70ae0e59811c1055ea
Russell Crawford <acrawford0221@gmail.com>
2023-03-11 11:24:37 -0700

    added all eventListner, but doesnt work

--- exercises/dj-js-evenrtListener-practice/app.js
+++ exercises/dj-js-evenrtListener-practice/app.js
@@ -1,11 +1,28 @@
 var square = document.getElementById("square")
 
-function squareHover() {
-    console.log("It works!")
+function mouseEnter() {
+    square.style.backgroundColor = "blue"
+}
+function mouseDown(){
+    square.style.backgroundColor = "red"
+}
+function mouseUp(){
+    square.style.backgroundColor = "yellow"
+}
+function dblClick(){
+    square.style.backgroundColor = "green"
+}
+function wheel(){
+    document.body.style.backgroundColor = "orange"
+}
+function keyPress(){
+    square.style.backgroundColor = "red"
 }
 
-function squareDblclick(){
-    document.square.style.backgroundColor = "red"
-}
 
-square.addEventListener("click", squareDblclick)
+square.addEventListener("mouseEnter", mouseEnter)
+square.addEventListener("mouseDown", mouseDown)
+square.addEventListener("mouseUp", mouseUp)
+square.addEventListener("dblClick", dblClick)
+square.addEventListener("wheel", wheel)
+square.addEventListener("keyPress", keyPress)
