f3fd091d30ab224fe17217159c2c853b4dcfd14b
Russell Crawford <acrawford0221@gmail.com>
2023-03-12 12:00:17 -0700

    initial commit

---
+++ exercises/dom-olympics-start/index.js
@@ -0,0 +1,9 @@
+var header = document.getElementById("header")
+header.style.height = "100px"
+header.textContent = "JavaScript Made This!!"
+header.style.textAlign = "center"
+header.style.fontSize = "30px"
+
+/*var newP = document.createElement("p")
+newP.textContent = "Rush Crawford Wrote this JavaScript"
+document.body.prepend(newP)*/
