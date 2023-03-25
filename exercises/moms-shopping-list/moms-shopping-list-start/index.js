6fc3063525a23bc04355d871834e706a32118bbb
Russell Crawford <acrawford0221@gmail.com>
2023-03-25 16:44:00 -0700

    edit button will pull up input box and change to save

--- exercises/moms-shopping-list/moms-shopping-list-start/index.js
+++ exercises/moms-shopping-list/moms-shopping-list-start/index.js
@@ -1,24 +1,49 @@
 let form = document.addItem
+let index = 0
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
-
     let name = form.title.value
-    form.title.value = ""
-
-let list = document.getElementById("list")
+    form.title.value = "";
 
 let div = document.createElement("div")
 let edit = document.createElement("button")
 let x = document.createElement("button")
-
-div.textContent = name
+let cur = index
+/*x.style.width = "50px"
+edit.style.width = "50px"
+x.style.marginLeft = "370px"
+edit.style.marginLeft = "370px"
+x.style.marginBottom = "25px"*/
 edit.textContent = "edit"
+div.textContent = name
 x.textContent = "X"
-document.getElementById("list").append(div, edit, x)
+edit.setAttribute("id", "edit"+index)
+div.setAttribute("id", "div"+index)
+let newLi = document.createElement("li")
+div.append(edit, x)
+
+document.getElementById("list").append(div)
 
 x.addEventListener("click", function(){
-    console.log("Works")
+    div.remove(div)
+    edit.remove(edit)
+    x.remove(x)
 })
+edit.addEventListener("click", function(){
+   let newInput = document.createElement("input")
+   newInput.textContent = "new name"
+   const id = "div"+cur
+   const divA = document.getElementById(id)
+   console.log(divA)
+   divA.append(newInput)
+   /*let editButton = document.createElement("button")
+   editButton.textContent = "Submit"
+   divA.append(editButton)*/
+   let saveButon = edit.textContent = "Save"
+
+})
+
+index++
 })
 
