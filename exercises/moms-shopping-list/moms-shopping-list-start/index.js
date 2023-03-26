7e76f7e9362d403f84c611664c9e570662a108fe
Russell Crawford <acrawford0221@gmail.com>
2023-03-26 12:21:00 -0700

    completed project

--- exercises/moms-shopping-list/moms-shopping-list-start/index.js
+++ exercises/moms-shopping-list/moms-shopping-list-start/index.js
@@ -10,17 +10,12 @@ let div = document.createElement("div")
 let edit = document.createElement("button")
 let x = document.createElement("button")
 let cur = index
-/*x.style.width = "50px"
-edit.style.width = "50px"
-x.style.marginLeft = "370px"
-edit.style.marginLeft = "370px"
-x.style.marginBottom = "25px"*/
 edit.textContent = "edit"
 div.textContent = name
 x.textContent = "X"
 edit.setAttribute("id", "edit"+index)
 div.setAttribute("id", "div"+index)
-let newLi = document.createElement("li")
+//let newLi = document.createElement("li")
 div.append(edit, x)
 
 document.getElementById("list").append(div)
@@ -32,15 +27,24 @@ x.addEventListener("click", function(){
 })
 edit.addEventListener("click", function(){
    let newInput = document.createElement("input")
-   newInput.textContent = "new name"
+   newInput.value = ""
    const id = "div"+cur
    const divA = document.getElementById(id)
    console.log(divA)
    divA.append(newInput)
-   /*let editButton = document.createElement("button")
-   editButton.textContent = "Submit"
-   divA.append(editButton)*/
-   let saveButon = edit.textContent = "Save"
+   let changeButton = document.createElement("button")
+   changeButton.textContent = "Change"
+   divA.append(changeButton)
+   changeButton.addEventListener("click", function(){
+
+    edit.textContent = "edit"
+    div.textContent = newInput.value
+        x.textContent = "X"
+    edit.setAttribute("id", "edit"+index)
+    div.setAttribute("id", "div"+index)
+    div.append(edit, x)
+
+})
 
 })
 
