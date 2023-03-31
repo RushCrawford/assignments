c19559f4d2d282df1223ce49e6f17fa2e6104fec
Russell Crawford <acrawford0221@gmail.com>
2023-03-31 06:56:24 -0700

    complete

--- exercises/npm-readline/main.js
+++ exercises/npm-readline/main.js
@@ -2,13 +2,19 @@ const readline = require("readline-sync")
 let num1 = readline.question("What is your first number? ")
 let num2 = readline.question("What is your second number? ")
 let operation = readline.question("Please enter operation; add, sub, mul, div. ")
-let sum = num1+num2;
+let sum = parseInt(num1) + parseInt(num2);
 let product = num1*num2;
 let quotient = num1/num2;
 let difference = num1-num2;
-console.log(difference)
 
-
-/*if (operation == "add") {
-  greeting = "Good day";
-}*/
+if (operation == "add") {
+  console.log(sum);
+} else if (operation == "sub") {
+  console.log(difference);
+} else if (operation == "mul") {
+  console.log(product);
+} else if (operation == "div") {
+  console.log(quotient);
+} else {
+    console.log("Input not recognized. Try again.")
+}
