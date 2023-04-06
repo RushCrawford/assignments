d8d3f3971577350dcc48acca25ec978a07177555
Russell Crawford <acrawford0221@gmail.com>
2023-04-05 19:48:04 -0700

    initial commit, prelim complete, part of bronze

---
+++ exercises/loop-olympics/main.js
@@ -0,0 +1,32 @@
+//PRELIMS
+
+/*for (var i = 0; i < 10; i++) {
+    console.log(i)
+}*/
+
+/*for (var i = 9; i >= 0; i--) {
+    console.log(i)
+}*/
+
+/*const fruit = ["banana", "orange", "apple", "kiwi"]
+
+for (var i = 0; i < fruit.length; i++) {
+    console.log(fruit[i])
+}*/
+
+//BRONZE
+
+let numArray = []
+for (var i = 0; i < 10; i++) {
+    let numArray = i
+    numArray.i
+    console.log(numArray)
+}
+
+/*for (var i = 0; i <= 100; i++) {
+    if (i % 2 === 0)
+    console.log(i)
+}*/
+
+
+
