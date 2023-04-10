e0a7aca34436fdc0a452cb94a94ff62327916a9d
Russell Crawford <acrawford0221@gmail.com>
2023-04-10 12:00:41 -0700

    second commit

--- exercises/employee-records/main.js
+++ exercises/employee-records/main.js
@@ -4,7 +4,13 @@ function Employee (name, jobTitle, salary) {
     this.jobTitle = jobTitle;
     this.salary = salary;
     this.status = "Full Time";
+    employees.push(Employee)
     this.printEmployeeForm = function () {
         console.log(employees)
     }
 }
+
+let rush = new Employee("Rush Crawford", "Developer", 1000000)
+let tay = new Employee("Taylor Crawford", "Life Coach", 1000000, )
+
+console.log(rush)
