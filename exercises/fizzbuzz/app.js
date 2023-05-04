bc66400ec78f4abfed45fac6c03944ed3a18a5d9
Russell Crawford <acrawford0221@gmail.com>
2023-05-03 19:16:56 -0700

    initial commit

---
+++ exercises/fizzbuzz/app.js
@@ -0,0 +1,14 @@
+let array = []
+
+for (var i = 0; i < 101; i++ ){  
+   array.push(i)
+    
+}
+
+for (var i = 0; i < array.length; i++){
+    if (i % 3 === 0){
+        console.log(i)
+
+    }
+}
+ 
