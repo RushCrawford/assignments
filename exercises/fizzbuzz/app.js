bae32e817cf153ba309ae20455e282eb5cbca6cd
Russell Crawford <acrawford0221@gmail.com>
2023-05-09 18:05:47 -0700

    removed first fizzbuzz and 0, still prints numbers that should be replaced

--- exercises/fizzbuzz/app.js
+++ exercises/fizzbuzz/app.js
@@ -1,10 +1,9 @@
-//let array = []
 let fizzBuzzCount = 0
 let fizzCount = 0
 let buzzCount = 0
 
-for (var i = 0; i < 101; i++ ){  
-  // array.push(i) 
+for (var i = 1; i <= 100; i++ ){  
+
    if ((i % 3 === 0) && (i % 5 === 0)) {
     fizzBuzzCount++
     console.log('fizzbuzz')
