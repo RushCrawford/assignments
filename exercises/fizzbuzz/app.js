909ebdb969e540d134ee0e27df890110b62e03b0
Russell Crawford <acrawford0221@gmail.com>
2023-05-09 18:27:32 -0700

    finished

--- exercises/fizzbuzz/app.js
+++ exercises/fizzbuzz/app.js
@@ -4,18 +4,17 @@ let buzzCount = 0
 
 for (var i = 1; i <= 100; i++ ){  
 
-   if ((i % 3 === 0) && (i % 5 === 0)) {
-    fizzBuzzCount++
-    console.log('fizzbuzz')
-   } else if ((i % 3 === 0) && (i % 5 != 0)) {
-    fizzCount++
-    console.log('fizz')
-   } else if ((i % 5 === 0) && (i % 3 != 0)) {
-    buzzCount++
-    console.log('buzz')
-   } 
-   console.log(i)
+    i % 3 === 0 && i % 5 === 0 ? fizzBuzzCount++ & console.log('fizzbuzz')
+    :
+    i % 3 === 0 && i % 5 != 0 ? fizzCount++ & console.log('fizz')
+    :
+    i % 5 === 0 && i % 3 != 0 ? buzzCount++ & console.log('buzz')
+    :
+    
+    console.log(i)
 }
+
+
 
 
  
