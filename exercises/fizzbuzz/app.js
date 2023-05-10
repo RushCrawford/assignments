681c8dd46cc721e948a16598629209344d6bd2a4
Russell Crawford <acrawford0221@gmail.com>
2023-05-09 17:59:06 -0700

    added fizz and buzz counters

--- exercises/fizzbuzz/app.js
+++ exercises/fizzbuzz/app.js
@@ -1,12 +1,21 @@
 let array = []
 let fizzBuzzCount = 0
+let fizzCount = 0
+let buzzCount = 0
 
 for (var i = 0; i < 101; i++ ){  
    array.push(i) 
    if ((i % 3 === 0) && (i % 5 === 0)) {
     fizzBuzzCount++
     console.log('fizzbuzz')
-   }
+   } else if ((i % 3 === 0) && (i % 5 != 0)) {
+    fizzCount++
+    console.log('fizz')
+   } else if ((i % 5 === 0) && (i % 3 != 0)) {
+    buzzCount++
+    console.log('buzz')
+   } 
+
 }
 
 /*for (var i = 0; i < array.length; i++){
