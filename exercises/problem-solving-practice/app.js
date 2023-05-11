6206fb004d89e3d36e81defdffac895ab33695eb
Russell Crawford <acrawford0221@gmail.com>
2023-05-10 18:20:41 -0700

    initial commit part 2

--- exercises/problem-solving-practice/app.js
+++ exercises/problem-solving-practice/app.js
@@ -1,4 +1,5 @@
-let arr = [4, 5, 6, 7, 8, 9, 10, 87];
+//Write a function that takes an array of numbers and returns the largest (without using Math.max())
+/*let arr = [4, 5, 6, 7, 8, 9, 10, 87];
 temp = 0;
 
 arr.forEach((element) => {
@@ -7,6 +8,11 @@ arr.forEach((element) => {
   }
 });
 
-console.log(`The largest number in the array: ${temp}`);
+console.log(`The largest number in the array: ${temp}`);*/
 
+//Write a function that takes an array of words and a character and returns each word that has that character present.
+let array = ['hello', '$how', '@rray', 'javascript']
 
+for (var i = 0; i < array.length; i++) {
+    array[i] 
+}
