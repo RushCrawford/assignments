665852aa749927564eadacef3a6e8601549da387
Russell Crawford <acrawford0221@gmail.com>
2023-06-05 18:01:30 -0700

    added enemies

--- exercises/colossal-rpg/app.js
+++ exercises/colossal-rpg/app.js
@@ -1,15 +1,27 @@
-const readline = require('readline-sync');
-let hp = 100
+/*const readline = require('readline-sync');
+let hp = 50
 let inventory = []
 
 const name = readline.question('What is your name? ')
-console.log(`Welcome to the game ${name}. You are a mouse. Your goal is to get to the cheese at the center of the maze. There are cats, snakes and moustraps to avoid.`)
+console.log(`Welcome to the game ${name}. You are a mouse. Your goal is to get to the cheese at the center of the maze. There are enemies to avoid so beware!`)
 
 const walk = function w(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+  }*/
 
-function startOver (){
+function Enemy (name, attack, health) {
+  this.name = name
+  this.attack = attack
+  this.heatlh = health
+}
+const cat = new Enemy("Tom Cat", "Claws of Death", 50)
+const snake = new Enemy("Jake the Snake", "Jaws of Death", 50)
+const mousetrap = new Enemy("Cheese Wedge", "Sudden Snap", 50)
+
+let enemies = [cat, snake, mousetrap]
+
+console.log(enemies)
+/*function startOver (){
   const sentence = "(Enter w to walk.)"
   const action = readline.question(sentence)
 
@@ -29,9 +41,13 @@ function startOver (){
       
 }
 
-
+startOver()*/
 
   /*function w(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }*/
+
+  //write a while loop that while the player health is greater than zero the game keeps going
+  //make a function that when the health drops to zero the game is over
+  //use constructor functions to build an array of classes for heros or for the enemies
 
