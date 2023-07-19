e76031d2bf4d04738714bc4a827fedffde0f9b9e
Russell Crawford <acrawford0221@gmail.com>
2023-07-19 11:15:39 -0700

    got the add function to work with help of instructor roy

--- level-2/simple-html-calculator/app.js
+++ level-2/simple-html-calculator/app.js
@@ -1,23 +1,41 @@
-const form = document.getElementById("form") //calculatorForm
+let paper = document.calculatorForm //get calculator form
+// const form = document.querySelector("#form")
+//console.log(paper)// show value of paper variable
 
-console.log(form)
-document.addEventListener('submit', function(event){
+//  let answer = paper["answer"]
+// // console.log(answer)
+ let addButton = paper["addButton"] // create a button to add values
+ //console.log(addButton)// Show button value
+
+ //let addOne = paper["addOne"].value
+//let addTwo = paper["addTwo"].value
+
+const inputBox1 = document.getElementById("inputBox1")//Get input boxes from html
+const inputBox2 = document.getElementById("inputBox2")//Get input boxes from html
+//console.log(inputBox1, inputBox2)
+
+paper.addEventListener('click', function(event){
 event.preventDefault()
-console.log(form)
-    let addOne = form.addOne.value
-    let addTwo = form.addTwo.value
-    // let sum = form.sum.value
+
+const num1 = inputBox1.value
+const num2 = inputBox2.value
+console.log(num1, num2)
+
+    const sum = Number(num1) + Number(num2)
+    console.log(sum)
+    // answer.value = sum.value
+    //console.log(sum)
+
     // form.addOne.value = ""
     // form.addTwo.value = ""
-    // sum = addOne + addTwo
-    this.alert("works")
 
-    // if event is the add button then add
 })
 
 // define vars
 
 // addition function
-const add = () => {
-    //return inputOne + inputTwo
-}
+// const add = () => {
+//     //return inputOne + inputTwo
+// }
+
+//getElementById("form") 
