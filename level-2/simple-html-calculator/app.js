ca0922f25fc727abb58eb3c929576804d4f0dfb0
Russell Crawford <acrawford0221@gmail.com>
2023-07-18 18:28:37 -0700

    initial commit

---
+++ level-2/simple-html-calculator/app.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("form") //calculatorForm
+
+console.log(form)
+document.addEventListener('submit', function(event){
+event.preventDefault()
+console.log(form)
+    let addOne = form.addOne.value
+    let addTwo = form.addTwo.value
+    // let sum = form.sum.value
+    // form.addOne.value = ""
+    // form.addTwo.value = ""
+    // sum = addOne + addTwo
+    this.alert("works")
+
+    // if event is the add button then add
+})
+
+// define vars
+
+// addition function
+const add = () => {
+    //return inputOne + inputTwo
+}
