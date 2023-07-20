84d7b7e86dd17473bc8db89c81240ee6b2b5ed3a
Russell Crawford <acrawford0221@gmail.com>
2023-07-19 19:21:18 -0700

    initial commit

---
+++ level-2/travel-form/app.js
@@ -0,0 +1,21 @@
+let travelForm = document.travel
+// let firstName = travelForm.firstname
+// document.addEventListner ("submit", (event) => {
+//     event.preventDefault()
+//     console.log(travelForm['first-name'].value)
+//     console.log(travelForm['last-name'].value)
+//     console.log(travelForm.age.value)
+//     console.log(travelForm.gender.value)
+//     // console.log(travelForm.)
+//     // console.log(travelForm.)
+//     // console.log(travelForm.)
+//     // console.log(travelForm.)
+// })
+
+document.addEventListener("submit", function(event){
+    event.preventDefault()
+        console.log(firstName)
+    // console.log(travelForm['last-name'])
+
+  });
+
