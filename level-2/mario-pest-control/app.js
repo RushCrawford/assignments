fa7fe85b57bcfb7ac75cf0bc49425e2178b95345
Russell Crawford <acrawford0221@gmail.com>
2023-07-20 16:11:15 -0700

    fix button link images and style

---
+++ level-2/mario-pest-control/app.js
@@ -0,0 +1,24 @@
+let inputBox1 = document.getElementById('goombas')
+let inputBox2 = document.getElementById('bob-ombs')
+let inputBox3 = document.getElementById('cheep-cheeps')
+let totalOwed = document.getElementById('total-owed')
+let button = document.querySelector("button")
+// document.getElementById("button")
+
+button.addEventListener("click", ()=>{
+    console.log('hellos')
+})
+
+// document.addEventListener("submit", () => {
+//     // event.preventDefault()
+//     console.log('hellos')
+//     // const num1 = inputBox1.value
+//     // const num2 = inputBox2.value
+//     // const num3 = inputBox3.value
+//     // const sum = Number(num1) + Number(num2) + Number(num3)
+//     // console.log(sum)
+//     // totalOwed.textContent = sum
+//     // inputBox1 = ''
+//     // inputBox2 = ''
+//     // inputBox3 = ''
+// })
