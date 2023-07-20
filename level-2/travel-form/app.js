2c2bed2a530229881f3df8f502b52928a858b0db
Russell Crawford <acrawford0221@gmail.com>
2023-07-20 12:07:29 -0700

    could use more styling, but complete

--- level-2/travel-form/app.js
+++ level-2/travel-form/app.js
@@ -1,24 +1,7 @@
 let travelForm = document.travel
-// let firstName = travelForm["first-name"] 
-// let lastName = travelForm["last-name"] 
-// let age = travelForm.age
-// let gender = travelForm.gender
-// document.addEventListner ("submit", (event) => {
-//     event.preventDefault()
-//     console.log(travelForm['first-name'].value)
-//     console.log(travelForm['last-name'].value)
-//     console.log(travelForm.age.value)
-//     console.log(travelForm.gender.value)
-//     // console.log(travelForm.)
-//     // console.log(travelForm.)
-//     // console.log(travelForm.)
-//     // console.log(travelForm.)
-// })
 
 document.addEventListener("submit", function(event){
     event.preventDefault()
-        // console.log(travelForm.gender.value)
-    // console.log(travelForm['last-name'])
     const checkedInputs = []
     
     for(let i = 0; i < travelForm["dietary-restrictions"].length; i++){
@@ -26,8 +9,19 @@ document.addEventListener("submit", function(event){
             checkedInputs.push(travelForm["dietary-restrictions"][i].value)
         }
     }
-console.log(checkedInputs)
-console.log(travelForm.destination.value)
 
+    // let message = ["First Name: " + travelForm['first-name'].value, "Last Name: " + travelForm['last-name'].value, "Age: " + travelForm.age.value, "Gender: " + travelForm.gender.value, "Destination: " + travelForm.destination.value, "Dietary Restrictions: " + checkedInputs]
+
+    let message = `First Name: ${travelForm['first-name'].value}\nLast Name: ${travelForm['last-name'].value}\nAge: ${travelForm.age.value}\nGender: ${travelForm.gender.value}\nDestination: ${travelForm.destination.value}\nDietary Restrictions: ${checkedInputs}`
+
+    alert(
+        message
+        // "First Name: " + travelForm['first-name'].value,
+        // "Last Name: " + travelForm['last-name'].value,
+        // "Age: " + travelForm.age.value,
+        // "Gender: " + travelForm.gender.value,
+        // "Destination: " + travelForm.destination.value,
+        // "Dietary Restrictions: " + checkedInputs,
+    )
   });
 
