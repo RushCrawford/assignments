5bda0f904c95431aea8ce7010f8a061520afaee2
Russell Crawford <acrawford0221@gmail.com>
2023-07-21 15:05:54 -0700

    need to style

--- level-2/mario-pest-control/app.js
+++ level-2/mario-pest-control/app.js
@@ -1,24 +1,19 @@
+let form = document["form"]
 let inputBox1 = document.getElementById('goombas')
 let inputBox2 = document.getElementById('bob-ombs')
 let inputBox3 = document.getElementById('cheep-cheeps')
-let totalOwed = document.getElementById('total-owed')
-let button = document.querySelector("button")
-// document.getElementById("button")
+let hiDisplay = document.getElementById('total-owed')
 
-button.addEventListener("click", ()=>{
-    console.log('hellos')
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const num1 = inputBox1.value * 5
+    const num2 = inputBox2.value * 7
+    const num3 = inputBox3.value * 11
+    const sum = Number(num1) + Number(num2) + Number(num3)
+    console.log(sum)
+    hiDisplay.textContent = `Mario has earned a total of ${sum} coins!`
+    inputBox1.value = ''
+    inputBox2.value = ''
+    inputBox3.value = ''
 })
 
-// document.addEventListener("submit", () => {
-//     // event.preventDefault()
-//     console.log('hellos')
-//     // const num1 = inputBox1.value
-//     // const num2 = inputBox2.value
-//     // const num3 = inputBox3.value
-//     // const sum = Number(num1) + Number(num2) + Number(num3)
-//     // console.log(sum)
-//     // totalOwed.textContent = sum
-//     // inputBox1 = ''
-//     // inputBox2 = ''
-//     // inputBox3 = ''
-// })
