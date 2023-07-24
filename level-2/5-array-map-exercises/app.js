32b694643065c4ebe111c6f627429e8838b5f914
Russell Crawford <acrawford0221@gmail.com>
2023-07-24 15:10:50 -0700

    initial commit

---
+++ level-2/5-array-map-exercises/app.js
@@ -0,0 +1,7 @@
+// 1) Make an array of numbers that are doubles of the first array
+    let numbersArray = [2, 5, 100]
+    // let doubleNumbers = numbersArray.map(nums => nums * 2)
+    // console.log(doubleNumbers); // [4, 10, 200]
+
+// 2) Take an array of numbers and make them strings
+    let stringNumbers = numbersArray.map(nums => nums)
