b4dba297c7b3413e06c102373212fb6d0c7c7547
Russell Crawford <acrawford0221@gmail.com>
2023-07-25 15:27:24 -0700

    initial commit, complete

---
+++ level-2/7-array-sort-exercises/app.js
@@ -0,0 +1,27 @@
+// 1) Sort an array from smallest number to largest
+let numbers = [1, 3, 5, 2, 90, 20]
+    // numbers.sort((a, b) => a - b)
+    // console.log(numbers)
+
+// 2) Sort an array from largest number to smallest
+    // numbers.sort((a, b) => b - a)
+    // console.log(numbers)
+
+// 3) Sort an array from shortest string to longest
+let strings = ["dog", "wolf", "by", "family", "eaten"]
+    // strings.sort((a,b) => a.length - b.length)
+    // console.log(strings)
+
+// 4) Sort an array alphabetically
+    // strings.sort()
+    // console.log(strings)
+
+// 5) Sort the objects in the array by age
+let people = [
+    { name: "Quiet Samurai", age: 22 },
+    { name: "Arrogant Ambassador", age: 100 },
+    { name: "Misunderstood Observer", age: 2 },
+    { name: "Unlucky Swami", age: 77 }
+]
+    // people.sort((a,b)=> a.age - b.age)
+    // console.log(people)
