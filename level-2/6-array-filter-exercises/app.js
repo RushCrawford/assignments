4d5ccb8adbac45057cf8bdc07cc7a344be77e255
Russell Crawford <acrawford0221@gmail.com>
2023-07-25 12:23:23 -0700

    initial commit, all works, complete

---
+++ level-2/6-array-filter-exercises/app.js
@@ -0,0 +1,35 @@
+// 1) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+// let numbers = [3, 6, 8, 2]
+// let greaterThanFive = numbers.filter(num => num > 5)
+// console.log(greaterThanFive)
+
+// 2) Given an array of numbers, return a new array that only includes the even numbers.
+// let evenNumbers = numbers.filter(num => num % 2 === 0)
+// console.log(evenNumbers)
+
+// 3) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
+// let words = ["dog", "wolf", "by", "family", "eaten", "camping"]
+// let fiveCharactersOrFewerOnly = words.filter(word => word.length < 5)
+// console.log(fiveCharactersOrFewerOnly)
+
+// 4) Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
+// let people = [
+//     { name: "Angelina Jolie", member: true },
+//     { name: "Eric Jones", member: false },
+//     { name: "Paris Hilton", member: true },
+//     { name: "Kayne West", member: false },
+//     { name: "Bob Ziroll", member: true }
+// ]
+// let members = people.filter(person => person.member)
+// console.log(members)
+
+// 5) Make a filtered list of all the people who are old enough to see The Matrix (older than 18)
+// let people = [
+//     { name: "Angelina Jolie", age: 80 },
+//     { name: "Eric Jones", age: 2 },
+//     { name: "Paris Hilton", age: 5 },
+//     { name: "Kayne West", age: 16 },
+//     { name: "Bob Ziroll", age: 100 }
+// ]
+// let oldEnough = people.filter(person => person.age >= 18)
+// console.log(oldEnough)
