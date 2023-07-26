a7e3035bc56ad7a5a76b12009ca783611b330e7b
Russell Crawford <acrawford0221@gmail.com>
2023-07-26 12:38:43 -0700

    initial commit

---
+++ level-2/9-multiple-array-methods-part-1/app.js
@@ -0,0 +1,46 @@
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+// 1. Returns a list of everyone older than 18, which is !!! use filter to grab all older than 18s
+// 2. sorted alphabetically by last name, and where !!! use sort to alphabetize
+// 3. each name and age is embedded in a string that looks like an HTML `<li>` element. !!! map to create new array
+
+const olderThan18 = peopleArray.filter(person => person.age > 18)
+const alphabeticallyByLatName = olderThan18.lastName.sort()
+
+console.log(alphabeticallyByLatName)
