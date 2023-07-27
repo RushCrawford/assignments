10c823b1aa8731a4116870dc1bab84dd9f5db7c4
Russell Crawford <acrawford0221@gmail.com>
2023-07-27 10:05:47 -0700

    initial commit, complete

---
+++ level-2/10-midlevel-code-challenges/app.js
@@ -0,0 +1,7 @@
+// let numbers = [34, 56, 3, 17, 45, 23, 4, 18]
+// let result = numbers.sort((a,b) => a - b)
+// console.log(result)
+
+// let strings = ['dog', 'legos', 'usa']
+// let result = strings.map(string => string.toUpperCase())
+// console.log(result)
