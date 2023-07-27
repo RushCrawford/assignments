4f93a763ba41de26f75b4d8ba7527a7784018ebb
Russell Crawford <acrawford0221@gmail.com>
2023-07-27 08:44:56 -0700

    complete

--- level-2/9-multiple-array-methods-part-1/app.js
+++ level-2/9-multiple-array-methods-part-1/app.js
@@ -40,7 +40,14 @@ var peopleArray = [
 // 2. sorted alphabetically by last name, and where !!! use sort to alphabetize
 // 3. each name and age is embedded in a string that looks like an HTML `<li>` element. !!! map to create new array
 
-const olderThan18 = peopleArray.filter(person => person.age > 18)
-const alphabeticallyByLatName = olderThan18.lastName.sort()
+const peopleArray18 = peopleArray.filter(person => person.age > 18)
+const sortedPeopleArray18 = peopleArray18.sort((a,b)=> {const nameA = a.lastName; const nameB = b.lastName
+    if(nameA > nameB){
+        return 1
+    } else if (nameA < nameB){
+        return -1
+    } else {return 0}
+})
+const finalPeopleArray = sortedPeopleArray18.map(person => {return `<li>${person.firstName} ${person.lastName} is ${person.age}</li>`})
 
-console.log(alphabeticallyByLatName)
+console.log(finalPeopleArray)
