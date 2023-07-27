4d28feb3e564c40da110d0b64506e1f02287ce96
Russell Crawford <acrawford0221@gmail.com>
2023-07-27 14:55:22 -0700

    initial commit

---
+++ level-2/11-catch-me-if-you-can/app.js
@@ -0,0 +1,13 @@
+// console.log(result)
+
+try {
+    function addNumbers(a,b){
+        return a+b
+    }
+    // const result = addNumbers(94,273)
+if(a && b != Number){
+    throw 'not a number'
+} else {console.log('thumbs up')}
+} catch(err){
+console.log(err)
+}finally{console.log('smooth sailin')}
