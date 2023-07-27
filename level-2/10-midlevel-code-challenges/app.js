bdadf3beb019a7fbe148e89199a2a05b40eac571
Russell Crawford <acrawford0221@gmail.com>
2023-07-27 15:21:11 -0700

    completed code challenges

--- level-2/10-midlevel-code-challenges/app.js
+++ level-2/10-midlevel-code-challenges/app.js
@@ -1,7 +1,24 @@
-// let numbers = [34, 56, 3, 17, 45, 23, 4, 18]
-// let result = numbers.sort((a,b) => a - b)
-// console.log(result)
+// // let numbers = [34, 56, 3, 17, 45, 23, 4, 18]
+//  let result = numbers.sort((a,b) => a - b)
+// // console.log(result)
 
 // let strings = ['dog', 'legos', 'usa']
-// let result = strings.map(string => string.toUpperCase())
+// //let result = strings.map(string => string.toUpperCase())
 // console.log(result)
+
+// function convertToUpper(string){
+//     return string.map(string => string.toUpperCase())
+// }
+
+// console.log(convertToUpper(strings))
+
+function filterEvenNumbers(numbers) {
+  return  numbers.filter(num => num % 2 === 0)
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const evenNumbers = filterEvenNumbers(numbers);
+console.log(evenNumbers);
+
+// filter 
+
