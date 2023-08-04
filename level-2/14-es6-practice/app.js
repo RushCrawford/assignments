4d324a5773116e9b6a80ae8d166fd521bbd2d05d
Russell Crawford <acrawford0221@gmail.com>
2023-08-04 13:46:44 -0700

    complete i think

--- level-2/14-es6-practice/app.js
+++ level-2/14-es6-practice/app.js
@@ -99,3 +99,43 @@ const people = [
 //  **Task 4**
 
 // Write a `printString` function that takes `firstName`, `lastName`, and `age` as parameters and returns a string like the following:
+
+    // const printString = (a,b,c) => {let firstName = a; let lastName = b; let age = c; return `Hi ${firstName} ${lastName}, how does it feel to be ${age}?`}
+    // console.log(printString('Trayson', 'Kelii', 30))
+
+//  **Task 5**
+
+// Use the shorthand syntax to make the following filter take up one line. Copy and paste the array to test it.
+const animals = [
+    {
+        type: "dog",
+        name: "theodore"
+    },
+    {
+        type: "cat",
+        name: "whiskers"
+    },
+    {
+        type: "pig",
+        name: "piglette"
+    },
+    {
+        type: "dog",
+        name: "sparky"
+    }
+ ];
+ 
+    // const filterForDogs = (arr) => {return arr.filter((animal) =>  { return animal.type === "dog" ? true : false})};
+
+    // // const myFunc = (param) => {param.filter(() => {})}
+    // console.log(filterForDogs(animals))
+
+
+// Using template literals, write a function that takes location and name parameters and outputs a message formatted like this:
+
+    // const printString = (name,location) => { return `Hi ${name}!
+
+    // Welcome to ${location}.
+
+    // I hope you enjoy your stay. Please ask the president of ${location} if you need anything.`}
+    // console.log(printString('Anthony','Maui'))
