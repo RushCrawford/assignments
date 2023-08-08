665d033a22f3e09662bebb9e9585591d635c2d89
Russell Crawford <acrawford0221@gmail.com>
2023-08-08 08:33:33 -0700

    initial commit

---
+++ level-2/15-slope-style/app.js
@@ -0,0 +1,7 @@
+// to help this function return an array of animals, no matter how many animals are passed to it:
+
+    // function collectAnimals(...animals) {
+    //     return animals
+    // }
+    // console.log(collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"))
+
