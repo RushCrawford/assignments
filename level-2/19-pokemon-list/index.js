73d089e0ddf74095463d0349a2cbe53b23b51316
Russell Crawford <acrawford0221@gmail.com>
2023-08-15 10:43:56 -0700

    cleaned up a bit

--- level-2/19-pokemon-list/index.js
+++ level-2/19-pokemon-list/index.js
@@ -7,19 +7,14 @@ xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
         const JSONdata = xhr.responseText;
         const data = JSON.parse(JSONdata);
-        console.log(data.objects[0].pokemon); 
         handleData(data.objects[0].pokemon); 
     }
 };
 
 function handleData(data) {
-    console.log(data); 
         const pokemonNames = data.map( pokemon => {
             const h1 = document.createElement('h1')
             h1.textContent = pokemon.name
             document.body.appendChild(h1) 
         })
 }
-
-// console.log(pokemonNames)
-
