31575b40555af50e05a7ce2c1a8a9405822cbb48
Russell Crawford <acrawford0221@gmail.com>
2023-08-16 12:31:17 -0700

    need to style and add more logic

--- level-2/axios-practice/app.js
+++ level-2/axios-practice/app.js
@@ -1,8 +1,9 @@
-axios.get('https://api.vschool.io/rush/todo')   //my attempt at adding a .map
+const todoForm = document.todoform
+
+axios.get('https://api.vschool.io/rush/todo')   //get request to display all items
     .then(response => {
-        console.log(response.data)
         response.data.map(todo => { 
-            const {title, description} = todo
+            const {title, description, price, imgUrl} = todo
 
             const h1 = document.createElement('h1')
             h1.textContent = title  
@@ -10,29 +11,41 @@ axios.get('https://api.vschool.io/rush/todo')   //my attempt at adding a .map
             const h3 = document.createElement('h3')
             h3.textContent = description
 
+            const priceH3 = document.createElement('h3')
+            priceH3.textContent = price
+
+            const img = document.createElement('img')
+            img.src = imgUrl;
+            img.style.height = '150px';
+            img.style.width = '200px';
+
             document.body.appendChild(h1);
-            document.body.appendChild(h3);}
-        )
+            document.body.appendChild(h3);
+            document.body.appendChild(img);
+            document.body.appendChild(priceH3);
+        })
     })
     .catch(error => {
         console.log(error)
-});
+    });
 
-// const newTodo = {
-//     title: 'Find a Bubbling Candle',
-//     description: 'Its actually called a Babylon Candle and they are very rare.',
-//     imgUrl: 'https://i.pinimg.com/736x/80/41/40/8041400275c1223cb0815d8ab1535ac8.jpg'
-// }
+todoForm.addEventListener('submit', function(event){    //submit event to add new todo items
+    event.preventDefault()
 
-// axios.post('https://api.vschool.io/rush/todo', newTodo)
-//     .then(response => console.log(response.data))
-//     .catch(error => console.log(error))
+    const newTodo = {
+        title: todoForm.title.value,
+        description: todoForm.description.value,
+        imgUrl: todoForm.imgUrl.value
+    }
 
+    axios.post('https://api.vschool.io/rush/todo', newTodo)
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+})
 
-
-
-//   axios.delete('https://api.vschool.io/rush/todo/64dc194678fe452c1149f436')
-//   .then(response => console.log(response.data))
-//   .catch(error => console.log(error))
-
-
+todoForm.delete.addEventListener('submit', function(event){
+    event.preventDefault()
+    axios.delete('https://api.vschool.io/rush/todo/64dc194678fe452c1149f436')   //make url dynamic
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+})
