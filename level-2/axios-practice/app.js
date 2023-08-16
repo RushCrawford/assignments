7de7ed6a04a1ee0a1e1a4d149a4250ca534dbcc1
Russell Crawford <acrawford0221@gmail.com>
2023-08-15 17:25:39 -0700

    get and post request successful

--- level-2/axios-practice/app.js
+++ level-2/axios-practice/app.js
@@ -1,7 +1,23 @@
-const axios = require("axios");
+const newTodo = {
+    title: 'Find a Bubbling Candle',
+    description: 'Its actually called a Babylon Candle and they are very rare.',
+    imgUrl: 'https://i.pinimg.com/736x/80/41/40/8041400275c1223cb0815d8ab1535ac8.jpg'
+}
+
+axios.post('https://api.vschool.io/rush/todo', newTodo)
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+
 axios.get('https://api.vschool.io/rush/todo')
     .then(response => {
-    console.log(response.data);
+        const {title, description} = response.data[0]
+        const h1 = document.createElement('h1')
+        const h1Des = document.createElement('h1')
+        h1.textContent = title  
+        h1Des.textContent = description
+        document.body.appendChild(h1, h1Des);
+        document.body.appendChild(h1Des);
   }).catch(error => {
     console.log(error)
   });
+
