b3a6936a16429549d06c5dfcd604fb49b2e3d7dd
Russell Crawford <acrawford0221@gmail.com>
2023-08-17 15:57:28 -0700

    restructuring to improve flow

--- level-2/axios-practice/app.js
+++ level-2/axios-practice/app.js
@@ -1,33 +1,63 @@
 const todoForm = document.todoform
+const todoList = document.getElementById('todo-list')
 
-axios.get('https://api.vschool.io/rush/todo')   //get request to display all items
-    .then(response => {
-        response.data.map(todo => { 
-            const {title, description, price, imgUrl} = todo
+getData = () => {
+    axios.get('https://api.vschool.io/rush/todo')
+    .then(res => listData(res.data))
+    .catch(err => console.log(err))
+}
 
-            const h1 = document.createElement('h1')
-            h1.textContent = title  
 
-            const h3 = document.createElement('h3')
-            h3.textContent = description
+listData = (todoArray) => {
+    todoArray.map(todo => {
+        const {title, imgUrl} = todo;
 
-            const priceH3 = document.createElement('h3')
-            priceH3.textContent = price
+        const liTitle = document.createElement('li');
+            liTitle.textContent = title;
 
-            const img = document.createElement('img')
+        const img = document.createElement('img');
             img.src = imgUrl;
             img.style.height = '150px';
             img.style.width = '200px';
 
-            document.body.appendChild(h1);
-            document.body.appendChild(h3);
-            document.body.appendChild(img);
-            document.body.appendChild(priceH3);
-        })
+        todoList.appendChild(liTitle)
+        todoList.appendChild(img)
     })
-    .catch(error => {
-        console.log(error)
-    });
+}
+
+getData()
+
+
+// axios.get('https://api.vschool.io/rush/todo')   //get request to display all items
+//     .then(response => {
+//         response.data.map(todo => { 
+
+//             const {title, description, price, imgUrl} = todo
+
+            // const liTitle = document.createElement('li')
+            // liTitle.textContent = title  
+
+//             const h3 = document.createElement('h3')
+//             h3.textContent = description
+
+//             const priceH3 = document.createElement('h3')
+//             priceH3.textContent = price
+
+            // const img = document.createElement('img')
+            // img.src = imgUrl;
+            // img.style.height = '150px';
+            // img.style.width = '200px';
+
+//             todoList.appendChild(liTitle);
+//             // todoList.appendChild(h3);
+//             // todoList.appendChild(priceH3);
+//             img.value !== '' && todoList.appendChild(img);     //attempt at if no img dont display anything
+
+//         })
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     });
 
 todoForm.addEventListener('submit', function(event){    //submit event to add new todo items
     event.preventDefault()
@@ -35,15 +65,22 @@ todoForm.addEventListener('submit', function(event){    //submit event to add ne
     const newTodo = {
         title: todoForm.title.value,
         description: todoForm.description.value,
-        imgUrl: todoForm.imgUrl.value
+        imgUrl: todoForm.imgUrl.value,
+        price: todoForm.price.value
     }
 
     axios.post('https://api.vschool.io/rush/todo', newTodo)
         .then(response => console.log(response.data))
         .catch(error => console.log(error))
+
+        todoForm.title.value = ''
+        todoForm.description.value = ''
+        todoForm.imgUrl.value = ''
+        todoForm.price.value = ''
+
 })
 
-todoForm.delete.addEventListener('submit', function(event){
+todoForm.delete.addEventListener('submit', function(event){     //delete button
     event.preventDefault()
     axios.delete('https://api.vschool.io/rush/todo/64dc194678fe452c1149f436')   //make url dynamic
         .then(response => console.log(response.data))
