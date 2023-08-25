cb99dea3341122ba496699de71bc67ee9c64d243
Russell Crawford <acrawford0221@gmail.com>
2023-08-25 12:59:08 -0700

    inital commoit

---
+++ level-3/2-airbnb-project/2-airbnb-project/src/components/Experiences.jsx
@@ -0,0 +1,8 @@
+export default function Experiences() {
+    return(
+        <div>
+            <h1>Experiences</h1>
+            <img className="card" src="./assets/image-12.png"/>
+        </div>
+    )
+}
---
+++ level-3/2-airbnb-project/2-airbnb-project/src/components/Nav.jsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+    return(
+        <nav className="nav">
+            <img className="nav--logo" src="https://www.edigitalagency.com.au/wp-content/uploads/airbnb-logo-png-transparent-background.png"/>
+        </nav>
+    )
+}
