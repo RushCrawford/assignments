33486c70faf6ffb080da43ccfef034deb9aac3dc
Russell Crawford <acrawford0221@gmail.com>
2023-08-25 14:17:15 -0700

    styled nav, photo grid, and one card

--- level-3/2-airbnb-project/2-airbnb-project/src/components/Experiences.jsx
+++ level-3/2-airbnb-project/2-airbnb-project/src/components/Experiences.jsx
@@ -1,8 +1,17 @@
 export default function Experiences() {
-    return(
-        <div>
-            <h1>Experiences</h1>
-            <img className="card" src="./assets/image-12.png"/>
+    return (
+        <div className="card">
+            <img className="card--img" src="https://www.athletespeakers.com/storage/celebrities/1627410490_Katie-Zaferes-Banner-1.jpg" />
+
+            <div className="rating">
+                <img className="card--star" src="https://www.freepnglogos.com/uploads/red-star-png/red-star-star-red-clip-art-clkerm-vector-clip-art-online-7.png" />
+                <p><span>5.0</span><span className="grey">(6)-USA</span></p>
+            </div>
+
+            <div className="price">
+                <p>Life lessons with Katie Zaferes</p>
+                <p><b>from $136</b> / person</p>
+            </div>
         </div>
     )
 }
