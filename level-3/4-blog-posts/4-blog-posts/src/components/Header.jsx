e815444bb8f32fd40a2fab44d6bfaa8044124227
Russell Crawford <acrawford0221@gmail.com>
2023-08-30 14:34:13 -0700

    header styled

--- level-3/4-blog-posts/4-blog-posts/src/components/Header.jsx
+++ level-3/4-blog-posts/4-blog-posts/src/components/Header.jsx
@@ -4,9 +4,11 @@ export default function Header() {
     return(
         <main className='header'>
             <img className='header--img' src='https://cdn.pixabay.com/photo/2018/01/11/21/27/desk-3076954_1280.jpg' />
-            <Navbar />
-            <h1 className='header--title'>Clean Blog</h1>
-            <h3 className='header--subtitle'>A Blog Theme by Start Bootstrap</h3>
+            <Navbar className='nav-component'/>
+            <div className='title-box'>
+                <h1 className='header--title'>Clean Blog</h1>
+                <h3 className='header--subtitle'>A Blog Theme by Start Bootstrap</h3>
+            </div>
         </main>
     )
 }
--- level-3/4-blog-posts/4-blog-posts/src/components/Navbar.jsx
+++ level-3/4-blog-posts/4-blog-posts/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 export default function Navbar() {
-    return(
+    return (
         <nav className="nav">
             <h3 className="nav--title">Start Bootstrap</h3>
-            <h4 className="nav--elements">HOME</h4>
-            <h4 className="nav--elements">ABOUT</h4>
-            <h4 className="nav--elements">SAMPLE POST</h4>
-            <h4 className="nav--elements">CONTACT</h4>
+            <div className="nav--elements-container">
+                <h4 className="nav--elements">HOME</h4>
+                <h4 className="nav--elements">ABOUT</h4>
+                <h4 className="nav--elements">SAMPLE POST</h4>
+                <h4 className="nav--elements">CONTACT</h4>
+            </div>
         </nav>
     )
 }
