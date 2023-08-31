2c2ea12ae4be145b472decf328a8bc8ae17a6f77
Russell Crawford <acrawford0221@gmail.com>
2023-08-31 09:12:27 -0700

    meets requirements

--- level-3/4-blog-posts/4-blog-posts/src/components/Footer.jsx
+++ level-3/4-blog-posts/4-blog-posts/src/components/Footer.jsx
@@ -1,10 +1,14 @@
 export default function Footer() {
-    return(
+    return (
         <footer className="footer">
-            <div className="footer--icons">twitter</div>
-            <div className="footer--icons">facebook</div>
-            <div className="footer--icons">github</div>
-            <small>Copyright © <a>rushdevelopment.com</a> 2023</small>
+            <div className="footer--icons-container">
+                <img src='https://seeklogo.com/images/T/twitter-icon-circle-black-logo-35827D553B-seeklogo.com.png'className="footer--icons" /> 
+
+                <img src='https://cdn-icons-png.flaticon.com/512/44/44646.png'className="footer--icons" /> 
+
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png'className="footer--icons" /> 
+            </div>
+            <small className="footer--copyright">Copyright © <a>rushdevelopment.com</a> 2023</small>
         </footer>
     )
 }
