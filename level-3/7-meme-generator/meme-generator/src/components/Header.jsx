5e161bbc21371623789e8db6d266345ab422069e
Russell Crawford <acrawford0221@gmail.com>
2023-09-07 12:57:50 -0700

    initial commit

---
+++ level-3/7-meme-generator/meme-generator/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="img-title">
+                <img className="header--img" src="https://www.pngmart.com/files/11/Rage-Comics-Troll-Face-PNG-File.png" />
+                <h3 className="header--title">Meme Generator</h3>
+            </div>
+            <h5 className="header--subtitle">React Course - Project 3</h5>
+        </header>
+    )
+}
