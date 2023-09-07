ab867da7523c406c32c431b47267306ef3907095
Russell Crawford <acrawford0221@gmail.com>
2023-09-07 13:30:56 -0700

    added inputs and button

---
+++ level-3/7-meme-generator/meme-generator/src/components/Inputs.jsx
@@ -0,0 +1,9 @@
+export default function Inputs() {
+    return (
+        <div>
+            <input type="text" placeholder="Top line of text"/>
+            <input type="text" placeholder="Bottom line of text"/>
+            <button>Get new meme image</button>
+        </div>
+    )
+}
--- level-3/7-meme-generator/meme-generator/src/main.jsx
+++ level-3/7-meme-generator/meme-generator/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header.jsx'
+import Inputs from './components/Inputs.jsx'
 import index from "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Inputs />
   </React.StrictMode>,
 )
