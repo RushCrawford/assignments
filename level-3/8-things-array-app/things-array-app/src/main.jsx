4c0bb9c62deb1ba63272792e341a0cbc74ba35a4
Russell Crawford <acrawford0221@gmail.com>
2023-09-08 15:33:08 -0700

    initial commit, also complete

---
+++ level-3/8-things-array-app/things-array-app/src/main.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function App() {
+
+const [thingsArray, setThingsArray] = useState(['Thing 1', 'Thing 2']) 
+
+  function addItem() {
+      setThingsArray(prev => [...prev, `Thing ${prev.length + 1}`])
+  }
+  
+  const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+  
+  return (
+      <div>
+          <button onClick={addItem}>Add Item</button>
+          {thingsElements}
+      </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>,
+)
