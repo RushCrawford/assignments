7f05e6b113795d0d0198f5bd7c1043bead6eb42a
Russell Crawford <acrawford0221@gmail.com>
2023-09-08 15:03:12 -0700

    styled inputs section

--- level-3/7-meme-generator/meme-generator/src/components/Inputs.jsx
+++ level-3/7-meme-generator/meme-generator/src/components/Inputs.jsx
@@ -1,8 +1,10 @@
 export default function Inputs() {
     return (
-        <div>
-            <input type="text" placeholder="Top line of text"/>
-            <input type="text" placeholder="Bottom line of text"/>
+        <div className="inputs-container">
+            <div className="inputs">
+                <input className="top-input" type="text" placeholder="Top line of text" />
+                <input className="bottom-input" type="text" placeholder="Bottom line of text" />
+            </div>
             <button>Get new meme image</button>
         </div>
     )
