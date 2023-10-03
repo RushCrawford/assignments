28812669101065228597dfb0ccbae770c2bb0e76
Russell Crawford <acrawford0221@gmail.com>
2023-10-03 10:48:02 -0700

    party dj works

--- level-3/12-dj-react/dj-react/src/App.jsx
+++ level-3/12-dj-react/dj-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import squaresArray from "./squaresArray"
-import Squares from './Squares'
+import Square from './Square'
 import Buttons from './Buttons'
 
 function App() {
@@ -19,25 +19,40 @@ function App() {
   }
 
   const Party = () => {
-    setColors( prevColor.map(color => {
-      if(prevColor.id > 2){
-        return 'purple'
-      } else if(indexOf(color) > 2) {
-        return color
-      }
-  }))
+    //   setColors( prevColor.map(color => {
+    //     if(prevColor.id > 2){
+    //       return 'purple'
+    //     } else if(indexOf(color) > 2) {
+    //       return color
+    //     }
+    // }))
     //  const purpleSquares = colors.filter((color) => color )
-    
-    //   setColors((prevColors) => ({
-    //     colors: [...prevColors.colors.slice(0,2),'purple', 'purple']
-    //   }));
+
+    // setColors((prevColors) => ({
+    //   colors: [...prevColors.slice(0, 2), 'purple', 'purple']
+    // }));
+      const tempColors = colors.slice(2, 4)
+      const newColors = ["purple", "purple", ...tempColors]
+      setColors(newColors)
+    console.log(colors)
     // };
-    
-      // setColors((prevColors) => prevColors.map((value, index) => (index < 2 ? 'purple' : value)));
+
+    // setColors((prevColors) => prevColors.map((value, index) => (index < 2 ? 'purple' : value)));
   }
 
-  const squareElements = squaresArray.map(square => (<Squares key={square.id} colors={colors} />))
+  // const squareElements = squaresArray.map(square => (
+    // <Square
+    //   key={square.id}
+    //   colors={colors}
+    //   index={indexOf(colors)}
+    // />
+  // ))
 
+  const squareElements = colors.map(color => (
+    <Square
+      color={color}
+    />
+  ))
 
   return (
     <>
@@ -45,9 +60,9 @@ function App() {
         {squareElements}
       </main>
       <div className='buttons-container'>
-        <Buttons 
-        smallTime={SmallTime} 
-        party={Party}
+        <Buttons
+          smallTime={SmallTime}
+          party={Party}
         />
       </div>
     </>
---
+++ level-3/12-dj-react/dj-react/src/Square.jsx
@@ -0,0 +1,11 @@
+export default function Square(props) {
+    // const styles = {
+    //     backgroundColor: colors
+    // }
+    // console.log(props.colors)
+    return (
+        <>
+            <div className="square" style={{backgroundColor: props.color}}></div>
+        </>
+    )
+}
