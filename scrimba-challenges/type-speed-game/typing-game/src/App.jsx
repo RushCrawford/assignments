29db8ca49cbd9cd05a4c98fcdec8d1dd4c5c4604
Russell Crawford <acrawford0221@gmail.com>
2023-11-10 12:59:51 -0700

    fully functional so far

--- scrimba-challenges/type-speed-game/typing-game/src/App.jsx
+++ scrimba-challenges/type-speed-game/typing-game/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 
 function App() {
+  const STARTING_TIME = 3
+
   const [text, setText] = useState('')
-  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
   const [isTimeRunning, setIsTimeRunning] = useState(false)
   const [wordCount, setWordCount] = useState(0)
 
@@ -17,6 +19,17 @@ function App() {
     return wordsArray.filter(word => word !== '').length;
   }
 
+  const startGame = () => {
+    setIsTimeRunning(true)
+    setText('')
+    setTimeRemaining(STARTING_TIME)
+    setWordCount(0)
+  }
+  
+  const endGame = () => {
+    setIsTimeRunning(false)
+    setWordCount(countWords(text))
+  }
   
   useEffect(() => {
     if(isTimeRunning && timeRemaining > 0) {
@@ -24,21 +37,23 @@ function App() {
         setTimeRemaining(time => time - 1)
       }, 1000)
     } else if(timeRemaining === 0) {
-      setIsTimeRunning(false)
-      setWordCount(countWords(text))
+      endGame()
     }
   }, [timeRemaining, isTimeRunning])  
 
   return (
     <>
       <h1>How fast can you type?</h1>
-      <textarea onChange={handleChange} value={text} />
-      <h4>time remaining: {timeRemaining}</h4>
-      <button onClick={() => {setIsTimeRunning(true)}}>Start</button>
+      <textarea 
+      onChange={handleChange} 
+      value={text} 
+      disabled={!isTimeRunning}
+      />
+      <h4>time remaining: {timeRemaining} sec</h4>
+      <button onClick={startGame} disabled={isTimeRunning}>Start</button>
       <h1>word count: {wordCount}</h1>
     </>
   )
 }
 
-//() => console.log(countWords(wordCount))
 export default App
