f0e032e44b457e48f03816901aed6c16ae6d1efe
Russell Crawford <acrawford0221@gmail.com>
2023-11-29 12:11:53 -0700

    need to sort alphabetically still

--- level-4/5-mid-level-code-challenges/mid-level-code-challenges/src/App.jsx
+++ level-4/5-mid-level-code-challenges/mid-level-code-challenges/src/App.jsx
@@ -1,19 +1,54 @@
 import { useState } from 'react'
 
-//Write a function called filterAnagrams that filters an array of words to find and return an array containing only the words that are anagrams of a given target word.
 
 function App() {
-  const [count, setCount] = useState(0)
+  //Write a function called filterAnagrams that filters an array of words to find and return an array containing only the words that are anagrams of a given target word.
 
-  function filterAnagrams(arr, target) {
-    arr.filter(word => word == target)
-  }
-  
-  const words = ['listen', 'silent', 'dog', 'god', 'hello', 'world'];
-  const target = 'enlist';
-  
-  const anagrams = filterAnagrams(words, target);
-  console.log(anagrams); // Output: ['listen', 'silent']
+    // function filterAnagrams(arr, target) {
+    //   const splitTarget = target.split('')
+
+    //   return arr.filter(word => {
+    //     const splitWords = word.split('');
+        
+    //     for (const char of splitTarget) {
+    //       if (!splitWords.includes(char)) return false;
+    //     }
+    
+    //     return true;
+    //   })
+    // }
+    
+    // const words = ['listen', 'silent', 'dog', 'god', 'hello', 'world'];
+    // const target = 'enlist';
+    
+    // const anagrams = filterAnagrams(words, target);
+    // console.log(anagrams); // Output: ['listen', 'silent']
+
+  //Write a function called sortByMultipleCriteria that takes an array of objects representing people, each with a name (string) and age (number) property. The function should return a new array with the people sorted first by age in ascending order, and then by name in alphabetical order.
+
+    function sortByMultipleCriteria(people) {
+      return (
+      people.sort((a,b)=> 
+      a.age - b.age
+      )
+    )}
+    
+    const people = [
+    { name: 'Alice', age: 30 },
+    { name: 'Bob', age: 25 },
+    { name: 'Charlie', age: 35 },
+    { name: 'David', age: 25 },
+    ];
+    
+    const sortedPeople = sortByMultipleCriteria(people);
+    console.log(sortedPeople);
+    
+    // Expected outcome: [
+    //  { name: 'Bob', age: 25 },
+    //  { name: 'David', age: 25 },
+    //  { name: 'Alice', age: 30 },
+    //  { name: 'Charlie', age: 35 }
+    // ]
 
   return (
     <>
