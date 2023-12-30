6bc521395b527ef7eab9aea7fef070bb5b9ec519
Russell Crawford <acrawford0221@gmail.com>
2023-12-30 10:58:28 -0700

    mvp

--- level-5/1-bounty-hunter/index.js
+++ level-5/1-bounty-hunter/index.js
@@ -1,5 +1,29 @@
 const express = require('express')
 const app = express()
+const { v4: uuidv4 } = require('uuid')
+
+// {
+//     "firstName": "Darth",
+//     "lastName": "Vader",
+//     "living": false,
+//     "bounty": 10000,
+//     "type": "sith"
+// }
+
+app.use(express.json())
+
+const bounties = []
+
+app.get('/bounty', (req, res)=> {
+    res.send(bounties)
+})
+
+app.post('/bounty', (req, res)=> {
+    const newBounty = req.body
+    newBounty._id = uuidv4()
+    bounties.push(newBounty)
+    res.send(`Bounty for ${newBounty.firstName} ${newBounty.lastName} successfully added.`)
+})
 
 
 app.listen(7200, ()=> {
