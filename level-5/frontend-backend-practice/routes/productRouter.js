54912b7b1b15f232decc1b094f70529722c9dd35
Russell Crawford <acrawford0221@gmail.com>
2024-01-08 16:08:47 -0700

    get one route added

--- level-5/frontend-backend-practice/routes/productRouter.js
+++ level-5/frontend-backend-practice/routes/productRouter.js
@@ -2,9 +2,32 @@ const express = require('express')
 const productRouter = express.Router()
 const { v4:uuidv4 } = require('uuid')
 
+const productList = [{
+    name: "Artisan Sourdough Loaf",
+    description: "Our signature loaf made at 90% hydration and 33% fresh ground Kamut flour.",
+    imageUrl: "http://www.myimage....",
+    price: 6.99,
+    _id: uuidv4()
+}]
+
+// GET AND POST ROUTES //
 productRouter.route('/')
     .get((req,res)=> {
-        res.send('hello')
+        res.send(productList)
+    })
+    .post((req,res)=> {
+        const newProduct = req.body
+        newProduct._id = uuidv4()
+        productList.push(newProduct)
+        res.send(`${newProduct.name} added to the product list`)
+    })
+
+// BY ID ROUTES //
+productRouter.route('/:productId')
+    .get((req,res)=> {
+        const productId = req.params.productId
+        const filteredProduct = productList.filter(product => productId === product._id)
+        res.send(filteredProduct)
     })
 
 
