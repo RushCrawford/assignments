071af532592ff1d4fbf8e4923096c1bceaa52e3a
Russell Crawford <acrawford0221@gmail.com>
2024-01-11 13:30:16 -0700

    need to get front and backend connected properly

--- level-5/5-bounty-hunter-pt3/client/bounty-hunter/src/App.jsx
+++ level-5/5-bounty-hunter-pt3/client/bounty-hunter/src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Bounty from './components/bounty'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bounty, setBounty] = useState([])
+
+  useEffect(()=> {
+    axios.get('/bounty')
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <>
+      <Bounty />
     </>
   )
 }
 
 export default App
+
+// const init = [{
+//   firstName: "",
+//   lastName: "",
+//   living: true,
+//   bounty: 0,
+//   type: "",
+// }]
---
+++ level-5/5-bounty-hunter-pt3/client/bounty-hunter/src/components/Bounty.jsx
@@ -0,0 +1,18 @@
+function Bounty(props) {
+    const { firstName, lastName, living, bounty, type } = props
+
+    return(
+        <div style={{ backgroundColor: living ? green : red }} className="bounty-wrapper">
+            <div className="bounty--name-wrapper">
+                <h1>{firstName}</h1>
+                <h1>{lastName}</h1>
+            </div>
+            <div className="bounty--type-amount-wrapper"> 
+                <h4>{type}</h4>
+                <h2>{bounty}</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Bounty
