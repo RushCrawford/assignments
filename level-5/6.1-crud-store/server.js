2392232ee1d898636da86d570d6d35b1fc09ead6
Russell Crawford <acrawford0221@gmail.com>
2024-01-19 13:23:46 -0700

    connected to the db

---
+++ level-5/6.1-crud-store/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const port = process.env.PORT || 7200
+
+//MIDDLEWARE
+app.use(express.json())
+app.use(morgan('dev'))
+
+//DB CONNECTION
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://acrawford0221:GBXB5BMNpklRyQsk@cluster0.m5clkiw.mongodb.net/?retryWrites=true&w=majority',
+    ()=> {console.log('Connected to the DB')}
+)
+
+
+
+app.listen(port, ()=> { console.log('The server is running on port 7200') })
