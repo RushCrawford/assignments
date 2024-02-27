aff0eee57349c1b8ed0241d8157e8bce36714040
Russell Crawford <acrawford0221@gmail.com>
2024-02-27 09:22:03 -0700

    backend folder adde,dependencies installed, server file mostly set up, db connection pending

---
+++ level-4/lvl-4-capstone/backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const { expressjwt } = require('express-jwt')
+const PORT = process.env.PORT
+const SECRET = process.env.SECRET
+const URI = process.env.URI
+
+// MIDDLEWARE //
+app.use(express.json());
+app.use(morgan('dev'));
+
+// ROUTES //
+// app.use('/auth', require('./routes/authRouter'))
+// app.use('/api', expressjwt({ secret: SECRET, algorithms: ['HS256'] }))
+// app.use('/api/issue', require('./routes/issueRouter'))
+// app.use('/api/comment', require('./routes/commentRouter'))
+
+// DB CONNECTION //
+// mongoose.set('strictQuery', true);
+// mongoose.connect(`mongodb+srv://acrawford0221:${PASSWORD}@cluster1.daj8kql.mongodb.net/`, ()=> {
+//     console.log('Connected to the DB')
+// })
+
+// ERROR HANDLER //
+app.use((err,req,res,next)=> {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+// PORT //
+app.listen(PORT, ()=> {
+    console.log('The server is running on port 7200')
+})
